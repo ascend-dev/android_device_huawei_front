dde2f69822eba2a8ca94ee124e1b79dc7191da34
huangqiwu <huangqiwu@xiaomi.com>
2013-07-28 10:21:15 +0800

    Fix build cm-10.2

--- device_front.mk
+++ device_front.mk
@@ -158,12 +158,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	SkLibTiJpeg_Test
 
-# for bugmailer
-PRODUCT_PACKAGES += send_bug
-PRODUCT_COPY_FILES += \
-	system/extras/bugmailer/bugmailer.sh:system/bin/bugmailer.sh \
-	system/extras/bugmailer/send_bug:system/bin/send_bug
-
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.setupwizard.enable_bypass=1 \
 	dalvik.vm.dexopt-flags=m=y \
